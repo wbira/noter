af3b7ec8962d6b166eb12077871f1497992ff40a
Waldemar Bira <waldek.bira@gmail.com>
2020-04-09 23:54:20 +0200

    Created lambda for writing notes to dynamodb

--- backend/catalog/create-notes/handler.js
+++ backend/catalog/create-notes/handler.js
@@ -1,14 +1,34 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const client = new AWS.DynamoDB.DocumentClient();;
+const TableName = process.env.TABLE_NAME
+
+function createResponse(body, statusCode) {
+    return {
+        body,
+        statusCode
+    }
+}
 
 exports.lambdaHandler = async (event, context) => {
     console.log("Event:", event)
     console.log("Ctx:", context)
+
+    const { note } = JSON.parse(event.body);
+    const item = {
+        hashKey: uuid(),
+        note
+    }
+    const params = {
+        TableName,
+        Item: item
+    };
+    let response
     try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
+        await client.put(params).promise()
+        response = createResponse(JSON.stringify(item), 201)
     } catch (err) {
         console.log(err);
         return err;
--- backend/catalog/fetch-notes/handler.js
+++ backend/catalog/fetch-notes/handler.js
@@ -1,11 +1,36 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+const client = new AWS.DynamoDB.DocumentClient();;
+const TableName = process.env.TABLE_NAME
+
+
+function createResponse(body, statusCode) {
+    return {
+        body,
+        statusCode
+    }
+}
 
 exports.lambdaHandler = async (event, context) => {
+    console.log("Event:", event)
+    const params = {
+        TableName,
+        KeyConditionExpression: 'hashKey = :hkey',
+        ExpressionAttributeValues: {
+            ':hkey': 'first'
+        }
+    }
+    let response
     try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
+        const result = await client.query(params).promise()
+        console.log("Result", result)
+        if (result.Items) {
+            response = createResponse(JSON.stringify({
+                notes: result.Items,
+            }), 200)
+        } else {
+            response = createResponse("Not Found", 404)
         }
     } catch (err) {
         console.log(err);
